50b7d9386e90a81e825d8f5682b5ca30cb34f7c0
Miller1999 <millerjavierarias@gmail.com>
2023-10-29 01:00:58 -0500

    Finished on Desktop

---
+++ app.js
@@ -0,0 +1,36 @@
+const buttons = document.querySelectorAll("button");
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+const main = document.getElementById("form");
+const message = document.getElementById("message");
+const formMessage = document.querySelector("span");
+const email = document.getElementById("mail");
+
+const buttonsList = Array.from(buttons);
+
+console.log(formMessage);
+buttonsList.forEach((button) => {
+	button.addEventListener("click", (e) => {
+		e.preventDefault();
+	});
+});
+
+buttonsList[1].addEventListener("click", () => {
+	window.location.reload();
+});
+
+input.addEventListener("blur", (e) => {
+	const mail = e.target.value;
+	if (e.target.validity.valid === false) {
+		form.classList.add("form--input-error");
+		formMessage.classList.add("form--message-error");
+	} else {
+		form.classList.remove("form--input-error");
+		formMessage.classList.remove("form--message-error");
+		buttonsList[0].addEventListener("click", () => {
+			main.classList.add("hidden");
+			message.classList.remove("hidden");
+		});
+	}
+	email.innerText = mail;
+});
